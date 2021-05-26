8447c9163a47bfcb022db25390a56aa7b5792f75
sumerian12345 <76828889+sumerian12345@users.noreply.github.com>
2021-05-25 22:46:04 -0600

    A03 upload

---
+++ a03/GetMeshPathsModule.py
@@ -0,0 +1,34 @@
+import rhinoinside
+import ghhops_server as hs
+import rhino3dm
+rhinoinside.load() #this is new. This allows us to import Rhino and Rhino.Geometry
+
+import Rhino 
+import Rhino.Geometry as rg
+hops = hs.Hops(app=rhinoinside) #this is new
+
+import networkx as nx
+import MeshPaths as mp
+
+@hops.component(
+    "/MeshPathWalker",
+    name="MeshPathWalker",
+    description=" Make a networkx graph with mesh vertices as nodes and mesh edges as edges ",
+    inputs=[
+        hs.HopsMesh("Mesh","M","Mesh to make networkx graph for"),
+        hs.HopsString("WeightMode ","W","Weight Mode"),
+        hs.HopsString("GraphType","T","Mesh Graph Type"),
+        hs.HopsLine("SLine","L", "Line to process"),
+        hs.HopsString("mode" ,"m", "Shortest Path Mode")
+    ],
+    outputs=[
+        hs.HopsPoint("ShortestPath", "SP" ,"Shortest Path",hs.HopsParamAccess.LIST),
+        hs.HopsString("FacesIndex", "I" ,"Faces indexes",hs.HopsParamAccess.LIST)
+    ]
+)
+
+def MeshPathWalker(mesh,weightMode,GraphType,SLine,mode):
+    return mp.meshwalker(mesh,weightMode,GraphType,SLine,mode)
+
+if __name__ == "__main__":
+    hops.start()
